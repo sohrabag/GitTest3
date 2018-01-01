f5c08394c7c5fac8b0bc0266f7d0f186b1335858
sohrabag <33716625+sohrabag@users.noreply.github.com>
2018-01-01 15:37:06 +0100

    Added class nulable

---
+++ ConsoleApp1/ConsoleApp1/nulable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class nulable
+    {
+        public  void nullableExamp()
+        {
+            int? num = null;
+
+            if (num.HasValue)
+            {
+                Console.WriteLine("num value is: {0}", num.Value);
+            }
+            else { Console.WriteLine("num value is: null"); }
+        }
+    }
+}
